2b1d44b12c3591f961815685ad6331f7d59b5e7b
abidalili <abidalili1995@gmail.com>
2023-03-02 00:27:27 +0200

    keypress event learning

---
+++ index.js
@@ -0,0 +1,11 @@
+//keypress listener(more example in notebook)
+var count = 0;
+
+document
+  .querySelector("textarea")
+  .addEventListener("keypress", function (event) {
+    count++;
+    var text = event.key;
+    document.querySelector("p").innerHTML =
+      "you have pressed  " + count + "  characters";
+  });
